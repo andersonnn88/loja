8e520b6810a0826b82d1fec7ec9e25f3d914784d
andersonnn88 <35690587+andersonnn88@users.noreply.github.com>
2018-04-29 18:23:06 -0300

    Add files via upload

---
+++ loja1.0/controllers/homeController.php
@@ -0,0 +1,16 @@
+<?php
+class homeController extends Controller {
+
+	private $user;
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        $dados = array();
+
+        $this->loadTemplate('home', $dados);
+    }
+
+}
